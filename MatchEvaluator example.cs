59321f31ce4d631ca85230f332dfc45078f9d688
jiangwest <jiangdm@aliyun.com>
2017-03-12 09:09:01 +0800

    Update MatchEvaluator example.cs

--- MatchEvaluator example.cs
+++ MatchEvaluator example.cs
@@ -1,7 +1,17 @@
+//抽取脚本中的函数定义
+//其中函数定义以 keyword function开始，格式: function Type funcName([Type param]*);
+//1. 提取函数
+//2. 删除 keyword function
+//3. 给每个函数变量添加编号
+// example I: function bool test(int a,int b, string c);
+//转换为 test(1 int a,2 int b,3 string c);
+// example II: function bool test();
+//转换为 bool test();
 class Program
 {
     static void Main(string[] args)
     {
+        //简单MatchEvaluator应用
         string s = "1 12 3 5";
         s = Regex.Replace(s, @"\d+", new MatchEvaluator(CorrectString), RegexOptions.Compiled | RegexOptions.IgnoreCase);
         Console.WriteLine(s);
@@ -11,7 +21,7 @@ class Program
         //2. delete function key
         //3. give number serial to each parameters of function
         //e.g. extract the string like "function test(...)"
-        string str = "int i1;       function test(int a,int b, string c);";
+        string str = "int i1;       function bool test(int a,int b, string c);";
         str = Regex.Replace(str, @"\sfunction\s+[^;]+", new MatchEvaluator(procFuncTest), RegexOptions.Compiled | RegexOptions.IgnoreCase);
         Console.WriteLine(str);
         Console.ReadLine();
@@ -25,30 +35,32 @@ class Program
         return matchValue;
     }
 
+    //处理function
     private static string procFuncTest(Match match)
     {
         string matchStr = match.Value;
-        string retStr;
-
-        //1. delete function key
-        matchStr = Regex.Replace(matchStr, @"\sfunction\s", "", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        string retStr;  //返回值
+        
+        //1. find & delete function key
+        matchStr = Regex.Replace(matchStr, @"\sfunction\s", "", RegexOptions.IgnoreCase);
+        //分割函数声明
         string[] split = matchStr.Split(new string[] { ",", "(",")" }, StringSplitOptions.RemoveEmptyEntries);
+        //处理函数名+(
         retStr = split[0] + "(";
 
-        foreach (string s in split)
-            Console.WriteLine(s);
-
+        //split[]从1~length-1 为函数中参数声明
         for (int i = 1; i < split.Length; i++)
         {
             if(i == split.Length - 1)
-                split[i] = i + " " + split[i];
+                split[i] = i + " " + split[i];  //最后一个参数后无需加
             else
                 split[i] = i + " " + split[i] +",";
 
             retStr += split[i];
         }
-
-        retStr += ")";
-
+        
+        retStr += ")";  //处理函数结尾
+        
         return retStr;
     }
+}
